068febcd9a710d1aad30e9c1d3d0e4cf97d37101
jie <s10814024@gm.cyut.edu.tw>
2022-01-18 23:26:11 +0800

    test7

--- resources/views/test.php
+++ resources/views/test.php
@@ -1,7 +1,4 @@
+<link rel="stylesheet" href="css/test.css" >
 <?php
-     require_once ("Smarty.class.php");
-     $smarty = new Smarty();
-     $smarty->assign("title", "測試用的網頁標題");
-     $smarty->assign("content", "測試用的網頁內容");
-     $smarty->display('index.html');
+echo "hi";
  ?>
