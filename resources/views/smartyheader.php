e887dab2b81095f8b81d1e711ae65bc953b3a90e
jie <s10814024@gm.cyut.edu.tw>
2022-01-19 20:17:18 +0800

    smartyttt

--- resources/views/smartyheader.php
+++ resources/views/smartyheader.php
@@ -5,6 +5,6 @@ require_once(dirname(__FILE__). '/lib/Smarty.class.php');
 $smarty = new Smarty();
 $smarty->caching = true;
 $smarty->cache_lifetime = 120;
-$smarty->template_dir = dirname(__FILE__).'/templates';
-$smarty->compile_dir = dirname(__FILE__).'/templates_c';
+$smarty->setTemplateDir(dirname(__FILE__).'/templates');
+$smarty-setCompileDir(dirname(__FILE__).'/templates_c');
 ?>
--- resources/views/test2.php
+++ resources/views/test2.php
@@ -1,8 +1,6 @@
 <?php
      require_once (dirname(__FILE__)."/smartyheader.php");
      $smarty = new Smarty();
-     $smarty->template_dir = dirname(__FILE__).'/templates';
-     $smarty->compile_dir = dirname(__FILE__).'/templates_c';
      $smarty->assign("title", "測試用的網頁標題");
      $smarty->assign("content", "測試用的網頁內容");
      $smarty->display('index.html');
