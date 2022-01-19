46149d0293dc6bbe8c57c6e52a6899cdfeca7c39
jie <s10814024@gm.cyut.edu.tw>
2022-01-19 20:22:37 +0800

    smarty-test

--- resources/views/test2.php
+++ resources/views/test2.php
@@ -1,6 +1,8 @@
 <?php
-     require_once (dirname(__FILE__)."/smartyheader.php");
+     require_once(dirname(__FILE__).'/lib/Smarty.class.php');
      $smarty = new Smarty();
+     $smarty->setTemplateDir(dirname(__FILE__).'/templates');
+     $smarty->setCompileDir(dirname(__FILE__).'/templates_c');
      $smarty->assign("title", "測試用的網頁標題");
      $smarty->assign("content", "測試用的網頁內容");
      $smarty->display('index.html');
