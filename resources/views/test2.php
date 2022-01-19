2d58d6ff636bfd2475630d41d4ad950472bfaab9
jie <s10814024@gm.cyut.edu.tw>
2022-01-19 19:22:03 +0800

    test2

--- resources/views/test2.php
+++ resources/views/test2.php
@@ -1,7 +1,7 @@
 <?php
-     //require_once (dirname(__FILE__));
-     echo dirname(__FILE__);
-     /*
+     //require_once ("./lib/Smarty.class.php");
+    echo file_exists("./lib/Smarty.class.php");
+    /*
      $smarty = new Smarty();
      $smarty->assign("title", "測試用的網頁標題");
      $smarty->assign("content", "測試用的網頁內容");
