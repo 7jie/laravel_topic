e5ca10344d9ed771941aba1294d5eb3c5c18f297
jie <s10814024@gm.cyut.edu.tw>
2022-01-19 19:50:29 +0800

    whyyy

--- resources/views/test2.php
+++ resources/views/test2.php
@@ -5,6 +5,6 @@
      $smarty->compile_dir = dirname(__FILE__).'/templates_c';
      $smarty->assign("title", "測試用的網頁標題");
      $smarty->assign("content", "測試用的網頁內容");
-     $smarty->display('index.html');
+     $smarty->display(dirname(__FILE__).'/templates'.'/index.html');
 
  ?>
