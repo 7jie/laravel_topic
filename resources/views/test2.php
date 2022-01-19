1a9922b5cb2d422227f756e4ef1d3a6a6b5fc027
jie <s10814024@gm.cyut.edu.tw>
2022-01-19 19:43:14 +0800

    hiii-smarty

--- resources/views/test2.php
+++ resources/views/test2.php
@@ -1,5 +1,5 @@
 <?php
-     require_once (dirname(__FILE__)."./lib/Smarty.class.php");
+     require_once (dirname(__FILE__)."/lib/Smarty.class.php");
      $smarty = new Smarty();
      $smarty->assign("title", "測試用的網頁標題");
      $smarty->assign("content", "測試用的網頁內容");
