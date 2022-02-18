eb8bd54cf8849e0da142d03453e4731b80b42943
jie <s10814024@gm.cyut.edu.tw>
2022-02-18 10:19:10 +0800

    sfvwg

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\testpy;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,3 +17,4 @@ Route::get('/', function () {
     return view('test2');
     
 });
+Route::get('/test', [testpy::class, 'testA']);
